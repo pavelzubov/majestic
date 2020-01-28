1e2a803d14a44f47680f4483923a8265550d2c17
pavelzubov <pasha224@yandex.ru>
2020-01-28 14:53:33 +0300

    Change http to https

--- server/index.ts
+++ server/index.ts
@@ -46,7 +46,7 @@ async function main() {
         playground: "/debug"
       },
       async () => {
-        const url = `http://localhost:${port}`;
+        const url = `https://localhost:${port}`;
         console.log(`⚡  Majestic v${pkg.version} is running at ${url} `);
 
         if (args.app) {
